8132e6dd50ee106567d064490d568298dadb1fea
d0nth4x <contact@pdrzewiecki.pl>
2020-01-14 14:36:34 +0100

    Shorten queries

--- routes/message_body.py
+++ routes/message_body.py
@@ -9,17 +9,16 @@ body = Blueprint('message_body', __name__)
 
 @body.route('/message/body/all', methods=['GET'])
 def get_all():
-    b = db.session.query(MessageBody).filter().order_by(
+    b = MessageBody.query.filter().order_by(
         desc(MessageBody.id)
     ).limit(1000)
 
-    return jsonify(template_list=[i.serialize for i in b.all()])
+    return jsonify(message_body_list=[i.serialize for i in b.all()])
 
 
 @body.route('/message/body/<body_id>', methods=['GET'])
 def get_one(body_id):
-    b = db.session.query(MessageBody).filter(MessageBody.id == body_id).first()
-
+    b = MessageBody.query.get(body_id)
     return jsonify(b.serialize)
 
 
